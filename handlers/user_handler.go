44fd64aefafa634f6f15cd0eaafc5327e0b9be5c
Masashi <masashi.nkys@gmail.com>
2024-09-13 08:38:38 +0900

    Log error on `GetUsers`

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"log"
 	"morpet-backend/services"
 	"net/http"
 
@@ -10,6 +11,7 @@ import (
 func GetUsers(c echo.Context) error {
 	users, err := services.GetAllUsers()
 	if err != nil {
+		log.Printf("Failed to get users: %v", err)
 		return c.JSON(http.StatusInternalServerError, map[string]string{
 			"error": "Owataaaaa",
 		})
