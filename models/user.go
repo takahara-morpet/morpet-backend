db3a1a2bc25cd2a06401b7721849eaead9f8e0b5
takaharayota531 <takahara.yota@gmail.com>
2024-09-18 17:02:33 +0900

    swaggerの設定

--- models/user.go
+++ models/user.go
@@ -1,7 +1,9 @@
 package models
 
 type User struct {
-	Id   int    `json:"Id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Id      int    `json:"Id"`
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Profile string `json:"profile"`
+	Gender  string `json:"gender"`
 }
